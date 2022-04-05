8c59c86551e3ec0ce1a2d5a18e63b60187d7b2c5
Rana Sarkar <rana.sarkarg3@gmail.com>
2022-04-05 23:28:28 +0530

    Register route got protected

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
-const {register, login, forgetpassword, resetpassword} = require('../controllers/auth');
+const { register, login, forgetpassword, resetpassword } = require('../controllers/auth');
+const { protect } = require('../middleware/auth');
 
-router.route('/register').post(register);
+router.route('/register').post(protect, register);
 
 router.route('/login').post(login);
 
--- routes/private.js
+++ routes/private.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const {getPrivateData, affilatelist, workentry, worklist} = require('../controllers/private');
-const {protect} = require('../middleware/auth');
+const { protect } = require('../middleware/auth');
 
 router.route('/').get(protect, getPrivateData);
 router.route('/affilate/:type').get(protect, affilatelist);
 router.route('/workentry').post(protect, workentry);
 router.route('/worklist').post(protect, worklist);
-
 
 module.exports = router;
